bd04fa2d89df2494a57450a41e5c09c93990c98d
kajalvai0 <kajalvai0@gmail.com>
2025-10-29 08:36:43 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
-
+// Google Sheets Configuration
+const CONFIG = {
+    GOOGLE_SHEETS: {
+        SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+        SHEET_ID: 'YOUR_GOOGLE_SHEET_ID'
+    },
+    SHEETDB: {
+        API_URL: 'https://sheetdb.io/api/v1/YOUR_SHEETDB_ID'
+    },
+    APP: {
+        NAME: 'কর্জে হাসানা',
+        VERSION: '1.0.0'
+    }
+};
